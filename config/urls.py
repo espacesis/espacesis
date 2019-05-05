8912305437cfb4f4f81e5f9235f8a9ea098c9684
jeanluc243 <jeanluc.kabulu@gmail.com>
2019-05-05 07:58:38 +0200

    adding description and more

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from django.views import defaults as default_views
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path("softday9/", TemplateView.as_view(template_name="pages/sofday.html"), name="softday"),
+    path("softday9/", TemplateView.as_view(template_name="pages/softday.html"), name="softday"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
